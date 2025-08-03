a9c62116ab4ca88f84a541bd7a72752ab93084aa
hamitmizrak <hamitmizrak@gmail.com>
2025-08-03 10:05:28 +0300

    jquery, autocomplete

--- template.js
+++ template.js
@@ -1,3 +1,92 @@
+/* 
+Project name: jquery with web design
+- Author  : MsC Hamit Mızrak
+- Version : V1.0.0
+- Description : 
+- Technology  : html5,css3,bootstrap, jquery, JS, TS, +ES5
+- Summary     : 
+*/
+
+// -----------------------------------------------------------
+/* ~~~~ JQUERY SEARCH ~~~~ */
+/* SEARCH */
+// ready :
+$(document).ready(function () {
+  const searchData = [
+    'Adana',
+    'Ağrı',
+    'Bursa',
+    'Bitlis',
+    'Denizli',
+    'Diyarbakır',
+    'Elazığ',
+    'Hatay',
+    'Sivas',
+    'Malatya',
+    'Van',
+  ];
+
+  $('#search_id').autocomplete({
+    source: searchData,
+  });
+});
+
+
+
+// -----------------------------------------------------------
+
+// -----------------------------------------------------------
+/* ~~~~ JQUERY SEARCH ~~~~ */
+/* SEARCH */
+$(document).ready(function () {});
+
+// -----------------------------------------------------------
+
+// -----------------------------------------------------------
+/* ~~~~ JQUERY SEARCH ~~~~ */
+/* SEARCH */
+$(document).ready(function () {});
+// -----------------------------------------------------------
+
+// -----------------------------------------------------------
+/* ~~~~ JQUERY SEARCH ~~~~ */
+/* SEARCH */
+
+// -----------------------------------------------------------
+
+// -----------------------------------------------------------
+/* ~~~~ JQUERY SEARCH ~~~~ */
+/* SEARCH */
+
+// -----------------------------------------------------------
+
+// -----------------------------------------------------------
+/* ~~~~ JQUERY SEARCH ~~~~ */
+/* SEARCH */
+
+// -----------------------------------------------------------
+
+// -----------------------------------------------------------
+/* ~~~~ JQUERY SEARCH ~~~~ */
+/* SEARCH */
+
+// -----------------------------------------------------------
+
+// -----------------------------------------------------------
+/* ~~~~ JQUERY SEARCH ~~~~ */
+/* SEARCH */
+
+// -----------------------------------------------------------
+
+// -----------------------------------------------------------
+/* ~~~~ JQUERY SEARCH ~~~~ */
+/* SEARCH */
+
+// -----------------------------------------------------------
+// -----------------------------------------------------------
+/* ~~~~ JQUERY SEARCH ~~~~ */
+/* SEARCH */
+
 // WOW animasyon başlatma
 new WOW({
   boxClass: 'wow',
@@ -7,6 +96,10 @@ new WOW({
   live: true,
 }).init();
 
+// -----------------------------------------------------------
+/* ~~~~ BOOTSTRAP: SCROLLSPY ~~~~ */
+/* SEARCH */
+
 // ScrollSpy başlatma
 document.addEventListener('DOMContentLoaded', () => {
   const scrollSpy = new bootstrap.ScrollSpy(document.body, {
@@ -15,26 +108,29 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+// -----------------------------------------------------------
+/* ~~~~ COUNTERUP2 ~~~~ */
+/* COUNTERUP2 */
 
 // COUNTERUP2
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   const counterUp = window.counterUp.default;
 
   const callback = (entries) => {
     entries.forEach((entry) => {
       const el = entry.target;
-      if (entry.isIntersecting && !el.classList.contains("counted")) {
+      if (entry.isIntersecting && !el.classList.contains('counted')) {
         counterUp(el, {
           duration: 2000,
           delay: 16,
         });
-        el.classList.add("counted");
+        el.classList.add('counted');
       }
     });
   };
 
   const observer = new IntersectionObserver(callback, { threshold: 0.5 });
-  document.querySelectorAll(".counter").forEach((counter) => {
+  document.querySelectorAll('.counter').forEach((counter) => {
     observer.observe(counter);
   });
 });
